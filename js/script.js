58de1e44996c2771a3258fd25d737223fecaa969
Laura Vargas <tinidomin58@gmail.com>
2023-11-27 19:58:06 -0500

    Longitud de Array

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//Acceder a los elementos de un Array
+document.write("<h3>ELEMENTOS  DE UN ARRAY</h3>")
+const numbers = [1, 2, 3, 4, 5]
+document.write("El Array es: "+numbers+"<br>")
+document.write("El Array en su primera posición es: "+numbers[0]+"<br>")
+document.write("El Array en la posición dos es: "+numbers[2]+"<br>")
+
+//Usar variables para acceder a los elementos de un Array
+let index = 4
+document.write("La variable index corresponde a: "+numbers[index]+"<br>")
+
+//Modificar elementos de un Array
+numbers[0] = 10
+numbers [2] = 30
+document.write("Modificación al Array en la primera posición: "+numbers[0]+"<br>")
+document.write("Modificación al Array en la segunda posición: "+numbers[2]+"<br>")
+document.write("El Array modificado es: "+numbers[1, 2, 3, 4, 5]+"<br>")
+
+//Longitud de un Array
+document.write("<h3>LA LONGITUD DE UN ARRAY</h3>")
+let frutas = ["manzanas", "pera", "plátano", "fresa"]
+document.write("El Array de frutas es: "+frutas+"<br>")
+document.write("El Array de frutas tiene una longitud de: "+frutas.length+"<br>")
+
